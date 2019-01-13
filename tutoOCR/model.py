39ef82ff016feb1471774bc3a27d5e7bcd58eb20
Pirate Captain <st_lerendu@yahoo.fr>
2019-01-13 19:39:07 +0100

    add property into class

--- tutoOCR/model.py
+++ tutoOCR/model.py
@@ -1,18 +1,38 @@
 import json
+import math
 
 class Agent:
     def say_hello(self, first_name):
         return "Bien le bonjour " + first_name + " !"
 
-    def __init__(self, **agent_attributes):
+    def __init__(self, position, **agent_attributes):
+        self.position = position
         for attr_name, attr_value in agent_attributes.items():
             setattr(self, attr_name, attr_value)
 
 
+class Position:
+    def __init__(self, longitude_degrees, latitude_degrees):
+        self.longitude_degrees = longitude_degrees
+        self.latitude_degrees = latitude_degrees
+
+    @property
+    def longitude(self):
+        return self.longitude_degrees * math.pi / 180
+
+    @property
+    def latitude(self):
+        return self.latitude_degrees * math.pi / 180
+
+
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
-        agent = Agent(**agent_attributes)
-        print(agent.agreeableness)
+        # pop because the value have to be in Position but not in Agent
+        latitude = agent_attributes.pop('latitude')
+        longitude = agent_attributes.pop('longitude')
+        position = Position(longitude, latitude)
+        agent = Agent(position, **agent_attributes)
+        print(agent.position.longitude)
 
 
 agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, "longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic", "extraversion": 1.051833688742943, "date_of_birth": "2005-01-10", "agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549, "internet": 'false', "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
