0755396bd516f6a4a50376e09a9d3e5c4e3a1693
Pirate Captain <st_lerendu@yahoo.fr>
2019-01-13 19:05:31 +0100

    pending

--- tutoOCR/model.py
+++ tutoOCR/model.py
@@ -1,8 +1,11 @@
 class Agent:
-    pass
+    def say_hello(self, first_name):
+        return "Bien le bonjour " + first_name + " !"
+
+    def __init__(self, agreeableness):
+        self.agreeableness = agreeableness
 
 
-first_agent = Agent()
-second_agent = Agent()
-
-print(first_agent, second_agent)
+agent = Agent(0)
+print(agent.say_hello("Celine"))
+print(agent.agreeableness)
