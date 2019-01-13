87bc4bde0aabb2c5be0613ce17e0351e76b0a483
Pirate Captain <st_lerendu@yahoo.fr>
2019-01-13 19:24:42 +0100

    add stars

--- tutoOCR/model.py
+++ tutoOCR/model.py
@@ -4,14 +4,14 @@ class Agent:
     def say_hello(self, first_name):
         return "Bien le bonjour " + first_name + " !"
 
-    def __init__(self, agent_attributes):
+    def __init__(self, **agent_attributes):
         for attr_name, attr_value in agent_attributes.items():
             setattr(self, attr_name, attr_value)
 
 
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
-        agent = Agent(agent_attributes)
+        agent = Agent(**agent_attributes)
         print(agent.agreeableness)
 
 
